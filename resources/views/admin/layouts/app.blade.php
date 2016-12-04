4b71cc50e6f2c395ef7986c6031a6aba7ffc974d
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2016-12-04 17:03:00 +0100

    Correction Issue #7

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Benjamin Brasseur</title>
+    <title>Administration</title>
     <link rel="stylesheet" type="text/css" href="{{ url('/lib/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ url('/res/adminStyle.css') }}">
     <link rel="stylesheet" href="{{ url('/lib/fontawesome/css/font-awesome.min.css') }}">
