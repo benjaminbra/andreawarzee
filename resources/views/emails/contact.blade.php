a3cb4d3eaa29927f6175be0cc51b4354d4b34782
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2017-01-07 12:52:17 +0100

    #14

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -4,9 +4,9 @@
   <div style="background:#ededed;border:1px solid grey;border-radius:2px;padding:5px;text-align:justify;font-family:monospace;">
     <h1>{{$title}} - {{ $user }}</h1>
     <p>Message envoyé le : {{ date('d/m/Y à G:i:s') }}</p>
-    @if(copie){
+    @if(copie)
       <p>Copie de votre demande de contact</p>
-    }
+    @endif
     <p style="max-width: 500px;font-size:20px;">
       {{$content}}
     </p>
