30b3009b2805483550db07a872a93e45d3892560
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2017-02-24 18:04:56 +0100

    Bug footer, size description, logo mobile

---
+++ bootstrap/config/genealabs-laravel-caffeine.php
@@ -0,0 +1,8 @@
+<?php
+
+//TODO CHANGE THE DOMAIN
+return [
+    'dripIntervalInMilliSeconds' => 300000,
+    'domain' => 'http://andreawarzee.dev/',
+    'route' => 'genealabs/laravel-caffeine/drip',
+];
