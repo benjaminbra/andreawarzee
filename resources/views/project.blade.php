f44e1e294d2730b8d9cbe7b702ce9b66501fc366
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2017-05-09 20:40:57 +0200

    Add target blank for links

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -46,7 +46,7 @@
     function urlify(text) {
       var urlRegex = /(https?:\/\/[^\s]+)/g;
       return text.replace(urlRegex, function(url) {
-          return '<a href="' + url + '">' + url + '</a>';
+          return '<a target="_blank" href="' + url + '">' + url + '</a>';
       })
     }
   </script>
